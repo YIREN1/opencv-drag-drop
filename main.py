8a887dfaf7f6930bf9e280730c32c77f8d00abc3
Ren, Yi <yi.ren@ADP.com>
2021-09-02 15:57:59 -0400

    :sparkles: opencv

---
+++ main.py
@@ -0,0 +1,78 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import cvzone
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = HandDetector(detectionCon=0.8)
+colorR = (255, 0, 255)
+
+
+class DragRect():
+    def __init__(self, posCenter, size=[200, 200]):
+        self.posCenter = posCenter
+        self.size = size
+
+    def update(self, cursor):
+        cx, cy = self.posCenter
+        w, h = self.size
+
+        # If the index finger tip is in the rectangle region
+        if cx - w // 2 < cursor[0] < cx + w // 2 and \
+                cy - h // 2 < cursor[1] < cy + h // 2:
+            self.posCenter = cursor
+
+
+def checkClick(fingers):
+    return fingers[0] and fingers[1] and fingers[2] and not fingers[3] and not fingers[4]
+
+
+rectList = []
+for x in range(5):
+    rectList.append(DragRect([x * 250 + 150, 150]))
+
+while True:
+    success, img = cap.read()
+    if img is None:
+        continue
+    img = cv2.flip(img, 1)
+    hands, img = detector.findHands(img)
+    if hands:
+        hand1 = hands[0]
+        lmList = hand1['lmList']
+
+        if lmList:
+
+            fingers = detector.fingersUp(hand1)
+            if checkClick(fingers):
+                cursor = lmList[8]  # index finger tip landmark
+                # call the update here
+                for rect in rectList:
+                    rect.update(cursor)
+
+    # Draw solid
+    # for rect in rectList:
+    #     cx, cy = rect.posCenter
+    #     w, h = rect.size
+    #     cv2.rectangle(img, (cx - w // 2, cy - h // 2),
+    #                   (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+    #     cvzone.cornerRect(img, (cx - w // 2, cy - h // 2, w, h), 20, rt=0)
+
+    # Draw Transperency
+    imgNew = np.zeros_like(img, np.uint8)
+    for rect in rectList:
+        cx, cy = rect.posCenter
+        w, h = rect.size
+        cv2.rectangle(imgNew, (cx - w // 2, cy - h // 2),
+                      (cx + w // 2, cy + h // 2), colorR, cv2.FILLED)
+        cvzone.cornerRect(imgNew, (cx - w // 2, cy - h // 2, w, h), 20, rt=0)
+
+    out = img.copy()
+    alpha = 0.5
+    mask = imgNew.astype(bool)
+    out[mask] = cv2.addWeighted(img, alpha, imgNew, 1 - alpha, 0)[mask]
+
+    cv2.imshow("Image", out)
+    cv2.waitKey(1)
